d6fc82bfb69a4bab7e0058c9b922d5e09a2dcf55
lhomnoir <lhomnoir@Debian-Boot.168.1.75>
2026-03-23 16:13:35 +0000

    v1 of test user

---
+++ test_user.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+get_username(){
+   local username=$1
+    
+    # If entry is empty return the current username
+    if [ -z $username ]; then
+        echo "USERNAME=$(whoami)"
+
+    else
+        if (grep "^$username:" /etc/passwd >&/dev/null) ; then
+            echo "The user $username exists"
+        else
+            echo "Error: user $username does not exists"
+        fi
+    fi
+}
+
+get_uid(){
+    local username=$1
+
+    # If entry is empty display the id of current user
+    if [ -z $username ]; then
+        echo "UID=$(whoami | id -u)"
+    
+    else
+        if (grep "^$username:" /etc/passwd >&/dev/null); then
+            echo "UID=$(id -u $username)"
+        else
+            echo "Error: user does not exists"
+
+        fi
+    fi
+}
+
+get_uid 
+
+main(){
+    
+    while true; do
+        echo "1 - Vérifier l'existence d'un utilisateur"
+        echo "2 - Connaître l'UID d'un utilisateur"
+        echo "q - Quitter"
+
+        echo -n "Option: "
+        read -r option
+        
+        case "$option" in
+            1)
+                echo -n "username: "
+                read -r username
+                get_username $username
+                ;;
+            
+            2)
+                echo -n "username: "
+                read -r username
+                get_uid $username
+                ;;
+            
+            q)
+                echo "Merci! Au revoir"
+                break
+                ;;
+            *)
+                echo "Saisie invalide!"
+                continue
+                ;;
+        esac
+
+    done
+
+}
+main
